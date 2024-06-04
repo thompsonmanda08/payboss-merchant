13f152356fa168f6e654bd6a5f07cd99cb5a40fb
Thompson Manda <thompsonmanda08@outlook.com>
2024-06-04 12:58:55 +0200

    chore: add bread crumb component

---
+++ src/components/base/BreadCrumb.jsx
@@ -0,0 +1,47 @@
+'use client'
+import { usePathname } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { ChevronRightIcon } from '@heroicons/react/24/outline'
+
+export default function BreadCrumb({ baseUrl = '/' }) {
+  const pathname = usePathname()
+  const [path, setPath] = useState([baseUrl])
+
+  useEffect(() => {
+    setPath([...pathname.split('/').filter((path) => path !== '')])
+  }, [pathname])
+
+  function handleBreadCrumbClick(idx) {
+    window.location.href = `${baseUrl}/${path
+      .slice(0, idx + 1)
+      .join('/')}`.replace('//', '/')
+  }
+
+  return (
+    <div className="flex w-full justify-between text-xs ">
+      <div className="flex pr-1 pt-1">
+        {path &&
+          path.map((segment, idx) => (
+            <div key={idx} className="flex items-center gap-1">
+              <Link
+                onClick={() => handleBreadCrumbClick(idx)}
+                href={`${baseUrl}/${path.slice(0, idx + 1).join('/')}`.replace(
+                  '//',
+                  '/',
+                )}
+                className={`${
+                  idx === path.length - 1 ? 'text-slate-700' : 'text-slate-400'
+                } cursor-pointer px-2 font-medium capitalize`}
+              >
+                {segment.replace(/-|%20/g, ' ')}
+              </Link>{' '}
+              {idx < path.length - 1 && (
+                <ChevronRightIcon className="h-3 w-3 text-slate-500 hover:text-primary" />
+              )}
+            </div>
+          ))}
+      </div>
+    </div>
+  )
+}
