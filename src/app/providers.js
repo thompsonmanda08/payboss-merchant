ee17b1da9a2ec803af147fe7da389d523095fc11
Thompson Manda <thompsonmanda08@outlook.com>
2024-07-22 10:30:21 +0200

    fix: add network check for user

--- src/app/providers.js
+++ src/app/providers.js
@@ -2,11 +2,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { NextUIProvider } from '@nextui-org/react'
+import { useNetwork } from '@/hooks/useNetwork'
+import { motion } from 'framer-motion'
 // import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
 const queryClient = new QueryClient()
 
 function Providers({ children }) {
+  const { online } = useNetwork()
   return (
     <QueryClientProvider client={queryClient}>
       {/* <NextThemesProvider
@@ -15,7 +18,29 @@ function Providers({ children }) {
         enableSystem
         disableTransitionOnChange
       > */}
-      <NextUIProvider>{children}</NextUIProvider>
+
+      <NextUIProvider>
+        {!online && (
+          <motion.div
+            whileInView={{
+              y: [-20, 1],
+              opacity: [0, 1],
+              transition: {
+                duration: 0.3,
+                easings: 'easeInOut',
+                stiffness: {
+                  type: 'spring',
+                  stiffness: 100,
+                },
+              },
+            }}
+            className="relative -top-1 left-0 right-0 z-[999] bg-red-500 p-2 text-xs font-bold capitalize text-white"
+          >
+            NETWORK ERROR: Check your internet connection and try again!
+          </motion.div>
+        )}
+        {children}
+      </NextUIProvider>
       {/* </NextThemesProvider> */}
 
       <ReactQueryDevtools initialIsOpen={false} />
