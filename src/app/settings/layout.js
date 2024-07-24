f7f87098166e7add320a0e62b8be4038d5fcfc96
Thompson Manda <thompsonmanda08@outlook.com>
2024-07-24 19:24:27 +0200

    create settings layout

---
+++ src/app/settings/layout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { SettingsSideBar } from '../../components/base/SettingsSideBar'
+import { TopNavBar } from '@/components/base'
+
+function SettingsLayout({ children }) {
+  return (
+    <main className="flex h-screen items-start justify-start overflow-hidden bg-background text-foreground">
+      <SettingsSideBar />
+      <div className="relative flex max-h-screen w-full flex-col overflow-y-auto p-8 pt-20 lg:static lg:pt-8">
+        {/* <TopNavBar /> */}
+        {children}
+      </div>
+    </main>
+  )
+}
+
+export default SettingsLayout
