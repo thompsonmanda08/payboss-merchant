2b89a12fdc1342eb9af79b412b09319bb5cdee86
Thompson Manda <thompsonmanda08@outlook.com>
2024-07-27 01:57:21 +0200

    quickfix

--- src/components/containers/Settings/account-verification/BusinessAccountDetails.jsx
+++ src/components/containers/Settings/account-verification/BusinessAccountDetails.jsx
@@ -1,7 +1,15 @@
+import Step1 from '@/components/forms/signup-fragments/Step1'
+import Step2 from '@/components/forms/signup-fragments/Step2'
 import React from 'react'
 
 function BusinessAccountDetails() {
-  return <div>BusinessAccountDetails</div>
+  return (
+    <div className="mr-auto flex flex-col gap-y-10">
+      <Step1 />
+      <Step2 />
+      
+    </div>
+  )
 }
 
 export default BusinessAccountDetails
