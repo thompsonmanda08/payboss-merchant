13230112b4d2cf25470dba288dd737a9d7ba927e
Thompson Manda <thompsonmanda08@outlook.com>
2024-07-30 00:13:55 +0200

    configure middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,9 @@
 import { NextResponse } from 'next/server'
-import { cookies } from 'next/headers'
-import { AUTH_SESSION } from './lib/constants'
-import { decrypt } from './lib/session'
+import {
+  getServerSession,
+  getWorkspaceIDs,
+  getWorkspaceSession,
+} from './lib/session'
 
 const PROTECTED_ROUTES = [
   // '/dashboard',
@@ -18,36 +20,55 @@ export async function middleware(request) {
   const { pathname } = request.nextUrl
   const url = request.nextUrl.clone() // REQUIRED FOR BASE ABSOLUTE URL
   const response = NextResponse.next()
+  const workspaces = (await getWorkspaceIDs()) || []
+  const session = await getServerSession()
+  const urlRouteParams = pathname.match(/^\/dashboard\/([^\/]+)\/?$/)
+
+  const isUserInWorkspace =
+    pathname.startsWith('/dashboard') && pathname.split('/').length >= 3
+
+  /**********USER MUCH CHOOSE A WORKSPACE TO SEE DASHBOARDS *********/
+  if (urlRouteParams && isUserInWorkspace) {
+    const workspaceID = urlRouteParams[1]
+    const userId = urlRouteParams[2]
+
+    // console.log('URL Parameter (workspaceID):', workspaceID)
+    // console.log('URL Parameter (userId):', userId)
+
+    if (!workspaces.includes(workspaceID)) {
+      url.pathname = '/workspaces'
+      return NextResponse.redirect(url)
+    }
+  }
+
+  // const workspaceID = workspace?.workspaceID || ''
+  const accessToken = session?.accessToken || ''
+  const isAuthPage =
+    pathname.startsWith('/login') || pathname.startsWith('/register')
+  // console.log(workspaceID, accessToken)
 
   // CHECK FOR PUBLIC AND PROTECTED ROUTES
   const isProtectedRoute = PROTECTED_ROUTES.includes(pathname)
   const isPublicRoute = PUBLIC_ROUTE.includes(pathname)
 
-  // DECRYPT COOKIE TO GET SESSION
-  const cookie = cookies().get(AUTH_SESSION)?.value
-  const session = await decrypt(cookie)
+  // IF NO ACCESS TOKEN AND PUBLIC ROUTE - ALLOW ACCESS
+  if (isPublicRoute && !accessToken) return response
 
-  // IF PAGE IS PROTECTED AND THERE IS NO USER SESSION - REDIRECT TO LOGIN
-  if (isProtectedRoute && !session) {
-    url.pathname = '/login'
+  // // IF PAGE IS PROTECTED AND THERE IS NO USER SESSION - REDIRECT TO LOGIN
+  // if (isProtectedRoute && !session) {
+  //   url.pathname = '/login'
+  //   return NextResponse.redirect(url)
+  // }
+
+  // IF THERE IS AN ACCESS TOKEN EXISTS - REDIRECT TO DASHBOARD
+  if (accessToken && isAuthPage) {
+    url.pathname = '/dashboard'
     return NextResponse.redirect(url)
   }
 
-  // IF THERE IS AN ACTIVE USER SESSION - REDIRECT TO DASHBOARD
-  if (
-    session?.accessToken &&
-    (pathname.startsWith('/login') || pathname.startsWith('/register'))
-  ) {
-    url.pathname = '/dashboard'
-
-    // USER CAN ONLY SEE THE SETTINGS PAGE IF NO WORKSPACE IS CHOSEN
-    if (
-      (session?.workspaceID == null || session?.workspaceID == undefined) &&
-      !pathname.startsWith('/settings')
-    ) {
-      url.pathname = '/workspaces'
-    }
-
+  // IF NO ACCESS TOKEN AT ALL>>> REDIRECT BACK TO AUTH PAGE
+  if (!accessToken && !isPublicRoute) {
+    url.pathname = '/login'
     return NextResponse.redirect(url)
   }
 
