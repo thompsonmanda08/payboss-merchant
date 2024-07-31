db5ef1d71297c734ac667b4df8d0c3f4282dc0f7
Thompson Manda <thompsonmanda08@outlook.com>
2024-07-31 16:10:59 +0200

    add change password to component

--- src/components/containers/profile/ProfileSecuritySettings.jsx
+++ src/components/containers/profile/ProfileSecuritySettings.jsx
@@ -1,19 +1,12 @@
 'use client'
-import { Card, Modal } from '@/components/base'
+import { Card } from '@/components/base'
 import { Switch } from '@nextui-org/switch'
 import useProfileStore from '@/context/profileStore'
-import useSettingsStore from '@/context/settingsStore'
+import ChangePasswordField from '@/components/base/ChangePassword'
+import { useState } from 'react'
 
 function ProfileSecuritySettings() {
-  const { user } = useProfileStore()
-  const { openEditModal, setOpenEditModal } = useSettingsStore()
-
-  function handleToggleModal() {
-    setOpenEditModal(!openEditModal)
-  }
-  function handleConfirm() {
-    handleToggleModal()
-  }
+  const [changePassword, setChangePassword] = useState(false)
 
   return (
     <Card className={'rounded-2xl bg-white/65 backdrop-blur-md'}>
@@ -28,71 +21,52 @@ function ProfileSecuritySettings() {
                 Update your security settings to protect your account and data.
               </p>
             </div>
-            {/* <button
-              type="button"
-              // USER CHANGES THE PAYMENT PHONE NUMBER TO GENERATE NEW QR CODE
-              onClick={handleToggleModal}
-              className="font-semibold text-primary hover:text-primary/85"
-            >
-              Update Details
-            </button> */}
           </div>
-
-          <dl className="mt-6 space-y-6 divide-y divide-gray-100 border-t border-gray-200 text-sm leading-6">
-            <div className="pt-6 sm:flex">
-              <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-                Password
-              </dt>
-              <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-                <div className="text-gray-900">************</div>
-                <button
-                  type="button"
-                  onClick={handleToggleModal}
-                  className="font-semibold text-primary hover:text-primary/80"
-                >
-                  Change
-                </button>
-              </dd>
+          <div className="mt-6 space-y-6 divide-y divide-gray-100 border-t border-gray-200 text-sm leading-6">
+            <div>
+              <div className="items-center pt-6 sm:flex">
+                <div className="flex flex-col font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
+                  Password
+                  <span className="text-xs font-normal text-slate-600">
+                    Change your password
+                  </span>
+                </div>
+                {!changePassword && (
+                  <>
+                    <span className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
+                      <p className="text-gray-900">************</p>
+                    </span>
+                    <button
+                      type="button"
+                      onClick={() => setChangePassword(true)}
+                      className="font-semibold text-primary hover:text-primary/80"
+                    >
+                      Change
+                    </button>
+                  </>
+                )}
+              </div>
+              <ChangePasswordField
+                updatePassword={changePassword}
+                setUpdatePassword={setChangePassword}
+              />
             </div>
-            <div className="pt-6 opacity-55 grayscale sm:flex">
-              <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
+            {/* *************************************** */}
+            <div className="items-center pt-6 opacity-50 grayscale sm:flex">
+              <p className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
                 2F Authentication
-              </dt>
-              <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
+              </p>
+              <div className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
                 <div className="font-medium text-gray-900">
                   [disabled] Coming Soon!
                 </div>
-                <Switch />
-              </dd>
+                <Switch isDisabled />
+              </div>
             </div>
-            <div className="pt-6 sm:flex">
-              <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-                Recovery Email
-              </dt>
-              <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-                <div className="text-gray-900">{user?.email || 'Not Set'}</div>
-                <button
-                  type="button"
-                  className="font-semibold text-primary hover:text-primary/80"
-                >
-                  Update
-                </button>
-              </dd>
-            </div>
-          </dl>
+            {/* *************************************** */}
+          </div>
         </div>
       </div>
-      {/* {openEditModal && (
-        <Modal
-          show={openEditModal}
-          onClose={handleToggleModal}
-          onConfirm={handleConfirm}
-          title={'Update Profile Details'}
-          infoText={'Ensure that your account information is up to date.'}
-        >
-          <div className="flex h-24 w-full bg-red-500"></div>
-        </Modal>
-      )} */}
     </Card>
   )
 }
