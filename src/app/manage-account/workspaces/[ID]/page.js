432d85e11e468c7f43a6c87e840c08b6a2176e43
Thompson Manda <thompsonmanda08@outlook.com>
2024-08-02 11:56:18 +0200

    create workspace modification page

---
+++ src/app/manage-account/workspaces/[ID]/page.js
@@ -0,0 +1,27 @@
+import WorkspaceSettings from '@/components/containers/workspace/WorkspacesSettings'
+import Spinner from '@/components/ui/Spinner'
+import React, { Suspense } from 'react'
+
+function WorkSpaceIDPage({ params }) {
+  const WorkSpaceID = params.id
+  console.log(WorkSpaceID)
+  return (
+    <Suspense fallback={<Spinner size={64} />}>
+      <div className="mx-auto flex w-full max-w-7xl flex-col gap-8">
+        <div>
+          <h2 className="heading-3 !font-bold tracking-tight text-gray-900 ">
+            Workspace Settings
+          </h2>
+          <p className=" text-sm text-slate-600">
+            Workspaces provide a structured way to group and manage services,
+            users, and transactions effectively.
+          </p>
+        </div>
+
+        <WorkspaceSettings WorkSpaceID={WorkSpaceID} />
+      </div>
+    </Suspense>
+  )
+}
+
+export default WorkSpaceIDPage
