f4fe86752a19619caecfefa30dcf4282b9e01dd3
Thompson Manda <thompsonmanda08@outlook.com>
2024-08-13 19:22:13 +0200

    add users API query functions

---
+++ src/app/_actions/user-actions.js
@@ -0,0 +1,118 @@
+'use server'
+import authenticatedService from '@/lib/authenticatedService'
+import { getUserSession } from '@/lib/session'
+
+export async function createNewUser(newUser) {
+  const session = await getUserSession()
+  const merchantID = session?.user?.merchantID
+
+  try {
+    const res = await authenticatedService({
+      method: 'POST',
+      url: `merchant/${merchantID}/user/new`,
+      data: newUser,
+    })
+
+    if (res.status == 201 || res.status == 200) {
+      return {
+        success: true,
+        message: res.message,
+        data: res.data,
+        status: res.status,
+      }
+    }
+
+    const response = res?.data || res
+
+    return {
+      success: false,
+      message: response?.error || response?.message,
+      data: null,
+      status: res.status,
+    }
+  } catch (error) {
+    console.error(error)
+
+    return {
+      success: false,
+      message: error?.response?.data?.error || 'Operation Failed!',
+      data: null,
+      status: error?.response?.status || error.status,
+    }
+  }
+}
+
+export async function getAllUsers() {
+  const session = await getUserSession()
+  const merchantID = session?.user?.merchantID
+
+  try {
+    const res = await authenticatedService({
+      url: `merchant/users/${merchantID}`,
+    })
+
+    if (res.status == 200) {
+      return {
+        success: true,
+        message: res.message,
+        data: res.data,
+        status: res.status,
+      }
+    }
+
+    const response = res?.data || res
+
+    return {
+      success: false,
+      message: response?.error || response?.message,
+      data: null,
+      status: res.status,
+    }
+  } catch (error) {
+    console.error(error)
+
+    return {
+      success: false,
+      message: error?.response?.data?.error || 'Operation Failed!',
+      data: null,
+      status: error?.response?.status || error.status,
+    }
+  }
+}
+
+export async function getUser(userID) {
+  const session = await getUserSession()
+  const merchantID = session?.user?.merchantID
+
+  try {
+    const res = await authenticatedService({
+      url: `merchant/user/${userID}`,
+    })
+
+    if (res.status == 200) {
+      return {
+        success: true,
+        message: res.message,
+        data: res.data,
+        status: res.status,
+      }
+    }
+
+    const response = res?.data || res
+
+    return {
+      success: false,
+      message: response?.error || response?.message,
+      data: null,
+      status: res.status,
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      success: false,
+      message: error?.response?.data?.error || 'Operation Failed!',
+      data: null,
+      status: error?.response?.status || error.status,
+    }
+  }
+}
