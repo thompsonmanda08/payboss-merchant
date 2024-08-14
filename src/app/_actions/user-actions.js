391e3823a484f9ca9f8ec753dd7dede24c33fc87
Thompson Manda <thompsonmanda08@outlook.com>
2024-08-14 17:33:12 +0200

    add user mapping to endpoints

--- src/app/_actions/user-actions.js
+++ src/app/_actions/user-actions.js
@@ -80,6 +80,51 @@ export async function getAllUsers() {
   }
 }
 
+export async function assignUsersToWorkspace(users, workspaceID) {
+  // const users = [
+  //   {
+  //     userID: 'string',
+  //     roleID: 'string',
+  //   },
+  // ]
+
+  try {
+    const res = await authenticatedService({
+      url: `merchant/workspace/user/mapping/${workspaceID}`,
+      method: 'POST',
+      data: {
+        users: [...users],
+      },
+    })
+
+    if (res.status == 200) {
+      return {
+        success: true,
+        message: res.message,
+        data: res.data,
+        status: res.status,
+      }
+    }
+
+    const response = res?.data || res
+
+    return {
+      success: false,
+      message: response?.error || response?.message,
+      data: null,
+      status: res.status,
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      success: false,
+      message: error?.response?.data?.error || 'Operation Failed!',
+      data: null,
+      status: error?.response?.status || error.status,
+    }
+  }
+}
+
 export async function getUser(userID) {
   const session = await getUserSession()
   const merchantID = session?.user?.merchantID
