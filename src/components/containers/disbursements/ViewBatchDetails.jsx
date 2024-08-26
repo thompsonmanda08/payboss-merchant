f9eac3e444d1e31c42b8786f938f043273432146
Thompson Manda <thompsonmanda08@outlook.com>
2024-08-27 01:08:04 +0200

    create batch details view

---
+++ src/components/containers/disbursements/ViewBatchDetails.jsx
@@ -0,0 +1,126 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import useCustomTabsHook from '@/hooks/useCustomTabsHook'
+import ValidationDetails from '@/components/containers/disbursements/ValidationDetails'
+import ApproverAction from '@/components/containers/disbursements/ApproverAction'
+import RecordDetailsViewer from '@/components/containers/disbursements/RecordDetailsViewer'
+import usePaymentsStore from '@/context/paymentsStore'
+import { Modal, ModalContent, ModalBody, ModalHeader } from '@nextui-org/react'
+import { CardHeader } from '@/components/base'
+import { useQueryClient } from '@tanstack/react-query'
+
+export const BATCH_DETAILS_STEPS = [
+  {
+    title: 'Bulk payment - File Record Validation',
+    infoText: 'Provide details for the payment action batch files',
+    step: 'Validation',
+  },
+  {
+    title: 'Bulk payment - Approval Status',
+    infoText: 'You need to be an administrator to approve bulk payments',
+    step: 'Approval',
+  },
+]
+
+export default function BatchDetailsPage({ isOpen, onClose, service }) {
+  const [currentStep, setCurrentStep] = useState(BATCH_DETAILS_STEPS[0])
+  const queryClient = useQueryClient()
+
+  // ** INITIALIZEs PAYMENT STATE **//
+  const {
+    openAllRecordsModal,
+    openValidRecordsModal,
+    openInvalidRecordsModal,
+    selectedService,
+    setSelectedService,
+    selectedBatch,
+    setSelectedBatch,
+    setOpenBatchDetailsModal,
+  } = usePaymentsStore()
+
+  const batchID = selectedBatch?.ID
+
+  const COMPONENT_LIST_RENDERER =
+    selectedBatch.status.toLowerCase() != 'submitted'
+      ? [<ApproverAction batchID={batchID} key={'step-5'} />]
+      : [
+          <ValidationDetails
+            key={'step-4'}
+            batchID={batchID}
+            navigateForward={goForward}
+          />,
+          <ApproverAction batchID={batchID} key={'step-5'} />,
+        ]
+
+  const {
+    activeTab,
+    currentTabIndex,
+    navigateTo,
+    navigateForward,
+    navigateBackwards,
+  } = useCustomTabsHook(COMPONENT_LIST_RENDERER)
+
+  function goForward() {
+    navigateForward()
+  }
+  function handleClose() {
+    setOpenBatchDetailsModal(false)
+    setSelectedBatch(null)
+    onClose()
+  }
+
+  useEffect(() => {
+    setCurrentStep(BATCH_DETAILS_STEPS[currentTabIndex])
+  }, [currentTabIndex])
+
+  useEffect(() => {
+    if (service) {
+      setSelectedService(service)
+    }
+
+    return () => {
+      queryClient.invalidateQueries()
+    }
+  }, [service])
+
+  return (
+    <>
+      {/* *************** IF TOP_OVER RENDERING IS REQUIRED *******************/}
+      {(openAllRecordsModal ||
+        openValidRecordsModal ||
+        openInvalidRecordsModal) && <RecordDetailsViewer batchID={batchID} />}
+      {/*********************************************************************** */}
+      {/************************* COMPONENT RENDERER *************************/}
+      <Modal
+        size={'5xl'}
+        isOpen={isOpen}
+        onClose={handleClose}
+        isDismissable={false}
+      >
+        <ModalContent>
+          <>
+            <ModalHeader className="flex gap-1">
+              <CardHeader
+                title={
+                  <>
+                    {currentStep.title}
+                    {
+                      selectedService && selectedBatch && (
+                        <span className="capitalize">
+                          {' '}
+                          ({selectedService} - {selectedBatch?.batch_name}){' '}
+                        </span>
+                      ) //ONLY FOR THE CREATE PAYMENTS PAGE
+                    }
+                  </>
+                }
+                infoText={currentStep.infoText}
+              />
+            </ModalHeader>
+            <ModalBody>{activeTab}</ModalBody>
+          </>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
