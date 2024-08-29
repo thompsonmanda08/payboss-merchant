9c15b7ba14d3d44b4e8e9a481bcef08e1a2c04f0
Thompson Manda <thompsonmanda08@outlook.com>
2024-08-29 20:49:18 +0200

    fix: change loader component in loading page

--- src/app/loading.js
+++ src/app/loading.js
@@ -5,10 +5,7 @@ export default function LoadingPage() {
   return (
     <div className="grid h-[85vh] w-full place-content-center place-items-center">
       <div className="flex flex-col items-center justify-center p-10 text-center">
-        <Loader size={60} color="#1B64CE" text={'  Please wait...'} />
-        {/* <p className="mt-8 max-w-sm break-words font-bold text-slate-800">
-          Please wait...
-        </p> */}
+        <Loader size={60} color="#1B64CE" loadingText={'Please wait...'} />
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -94,20 +94,20 @@ export const theme = {
       sm: 'calc(var(--radius) - 4px)',
       '4xl': '2rem',
     },
-    // keyframes: {
-    //   'accordion-down': {
-    //     from: { height: '0' },
-    //     to: { height: 'var(--radix-accordion-content-height)' },
-    //   },
-    //   'accordion-up': {
-    //     from: { height: 'var(--radix-accordion-content-height)' },
-    //     to: { height: '0' },
-    //   },
-    // },
-    // animation: {
-    //   'accordion-down': 'accordion-down 0.2s ease-out',
-    //   'accordion-up': 'accordion-up 0.2s ease-out',
-    // },
+    keyframes: {
+      'accordion-down': {
+        from: { height: '0' },
+        to: { height: 'var(--radix-accordion-content-height)' },
+      },
+      'accordion-up': {
+        from: { height: 'var(--radix-accordion-content-height)' },
+        to: { height: '0' },
+      },
+    },
+    animation: {
+      'accordion-down': 'accordion-down 0.2s ease-out',
+      'accordion-up': 'accordion-up 0.2s ease-out',
+    },
   },
 }
 
