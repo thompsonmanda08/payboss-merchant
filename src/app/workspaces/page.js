d2ba464542f40c343d9535b99f02c70b6cfaacfb
Thompson Manda <thompsonmanda08@outlook.com>
2024-09-05 18:28:05 +0200

    quick fix 9

--- src/app/workspaces/page.js
+++ src/app/workspaces/page.js
@@ -1,54 +1,58 @@
 import { Card, InfoBanner } from '@/components/base'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 import { Workspaces } from '@/components/containers'
 import Image from 'next/image'
 import { DefaultCover } from '@/lib/constants'
 import WorkspaceHeader from '@/components/containers/workspace/WorkspaceHeader'
 import { getUserDetails } from '../_actions/config-actions'
+import LoadingPage from '../loading'
 
 async function WorkSpacesPage() {
   const session = await getUserDetails()
 
   return (
-    <main className="flex h-full min-h-screen items-start justify-start overflow-hidden bg-background text-foreground">
-      <div className="flex h-full max-h-screen w-full flex-col">
-        <section role="workspace-header">
-          <div className="relative h-[380px] w-full overflow-clip rounded-b-3xl bg-gray-900">
-            <WorkspaceHeader />
-            <Image
-              className="z-0 h-full w-full object-cover"
-              src={DefaultCover}
-              width={1024}
-              height={300}
-            />
+    <Suspense fallback={<LoadingPage />}>
+      <main className="flex h-full min-h-screen items-start justify-start overflow-hidden bg-background text-foreground">
+        <div className="flex h-full max-h-screen w-full flex-col">
+          <section role="workspace-header">
+            <div className="relative h-[380px] w-full overflow-clip rounded-b-3xl bg-gray-900">
+              <WorkspaceHeader user={session?.user} />
+              <Image
+                className="z-0 h-full w-full object-cover"
+                src={DefaultCover}
+                width={1024}
+                height={300}
+              />
 
-            <div className="absolute inset-0 z-10 bg-black/30"></div>
-          </div>
-        </section>
-
-        <section
-          role="workspace-header"
-          className="z-20 mx-auto -mt-40 w-full max-w-[1440px] px-5 md:px-10"
-        >
-          <Card className="gap-6">
-            <div className="flex justify-between bg-red-500/0">
-              <div>
-                <h2 className="heading-3 !font-bold tracking-tight text-gray-900 ">
-                  Choose a Workspace
-                </h2>
-                <p className=" text-sm text-slate-600">
-                  Access your account through a workspace for the convenience of
-                  having all your tools and resources organized in one place.
-                </p>
-              </div>
+              <div className="absolute inset-0 z-10 bg-black/30"></div>
             </div>
+          </section>
 
-            <Workspaces user={session} />
-          </Card>
-        </section>
-      </div>
-    </main>
+          <section
+            role="workspace-header"
+            className="z-20 mx-auto -mt-40 w-full max-w-[1440px] px-5 md:px-10"
+          >
+            <Card className="gap-6">
+              <div className="flex justify-between bg-red-500/0">
+                <div>
+                  <h2 className="heading-3 !font-bold tracking-tight text-gray-900 ">
+                    Choose a Workspace
+                  </h2>
+                  <p className=" text-sm text-slate-600">
+                    Access your account through a workspace for the convenience
+                    of having all your tools and resources organized in one
+                    place.
+                  </p>
+                </div>
+              </div>
+
+              <Workspaces user={session?.user} />
+            </Card>
+          </section>
+        </div>
+      </main>
+    </Suspense>
   )
 }
 
--- src/components/containers/workspace/WorkspaceHeader.jsx
+++ src/components/containers/workspace/WorkspaceHeader.jsx
@@ -6,21 +6,25 @@ import { Cog6ToothIcon, PowerIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
 import React from 'react'
 
-function WorkspaceHeader() {
+function WorkspaceHeader({ user }) {
   const { handleUserLogOut } = useAuthStore((state) => state)
+
   return (
     <>
       <Logo isWhite className="absolute left-5 top-5 z-30 md:left-10 " />
       <div className="absolute right-5 top-5 flex gap-4 md:right-10">
-        <Button
-          as={Link}
-          href={'/manage-account'}
-          variant="light"
-          className="data[hover=true]:bg-slate-900/30  z-30 aspect-square min-w-[120px] rounded-full bg-slate-900/20 text-white"
-          startContent={<Cog6ToothIcon className=" h-6 w-6" />}
-        >
-          Manage
-        </Button>
+        {(user.role?.toLowerCase() == 'admin' ||
+          user.role?.toLowerCase() == 'owner') && (
+          <Button
+            as={Link}
+            href={'/manage-account'}
+            variant="light"
+            className="data[hover=true]:bg-slate-900/30  z-30 aspect-square min-w-[120px] rounded-full bg-slate-900/20 text-white"
+            startContent={<Cog6ToothIcon className=" h-6 w-6" />}
+          >
+            Manage
+          </Button>
+        )}
         <Button
           onClick={handleUserLogOut}
           variant="light"
