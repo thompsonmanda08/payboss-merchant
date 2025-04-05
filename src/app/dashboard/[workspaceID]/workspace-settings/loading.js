82746f2c2e2a08009fc39a183a57b43f22d2e5f0
Thompson Manda <thompsonmanda08@outlook.com>
2025-04-06 00:09:03 +0200

    hot fix: patch update

---
+++ src/app/dashboard/[workspaceID]/workspace-settings/loading.js
@@ -0,0 +1,12 @@
+"use client";
+import Loader from "@/components/ui/loader";
+
+export default function LoadingPage({ loadingText = "Settings loading..." }) {
+  return (
+    <div className="grid h-[85vh] w-full place-content-center place-items-center">
+      <div className="flex flex-col items-center justify-center p-10 text-center">
+        <Loader color="#1B64CE" loadingText={loadingText} size={60} />
+      </div>
+    </div>
+  );
+}
