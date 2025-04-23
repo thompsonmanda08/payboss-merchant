949e8165d81119fefe7dfa3c1ef913230ed83f89
Thompson Manda <thompsonmanda08@outlook.com>
2025-04-23 11:08:23 +0200

    fix: add meta tag for security----sentry SSL conflict

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,13 +11,16 @@ const inter = localFont({
   variable: "--font-inter",
 });
 
+const description =
+  "PayBoss offers cutting-edge digital tools designed to help businesses of all sizes efficiently manage their financial inflows and outflows";
+
 export const metadata = {
   title: {
     template: "%s - PayBoss",
     default: "PayBoss - Gateway to simplified payments",
   },
-  description:
-    "PayBoss offers cutting-edge digital tools designed to help businesses of all sizes efficiently manage their financial inflows and outflows",
+  description,
+  manifest: "/manifest.json",
 };
 
 export default async function RootLayout({ children }) {
@@ -33,12 +36,39 @@ export default async function RootLayout({ children }) {
         <link href="/manifest.json" rel="manifest" />
         <meta content="width=device-width, initial-scale=1.0" name="viewport" />
         <meta content="#da532c" name="msapplication-TileColor" />
-        <meta content="#ffffff" name="theme-color" />
+
+        <meta
+          content="#ffffff"
+          media="(prefers-color-scheme: light)"
+          name="theme-color"
+        />
+        <meta
+          content="#000000"
+          media="(prefers-color-scheme: dark)"
+          name="theme-color"
+        />
+        <meta
+          content="upgrade-insecure-requests"
+          http-equiv="Content-Security-Policy"
+        />
+        <link href="/apple-icon.png" rel="apple-touch-icon" />
+        {/* Structured data */}
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "WebApplication",
+            name: "BGS PayBoss",
+            description,
+            url: "https://bgspayboss.com",
+            applicationCategory: "Marketplace",
+            operatingSystem: "Web",
+          })}
+        </script>
       </head>
       <body
         className={cn(
           "flex min-h-screen flex-col bg-background font-inter overflow-x-hidden",
-          inter.variable,
+          inter.variable
         )}
       >
         <Providers authSession={authSession} session={session}>
