30e555105aa1466b1264f31c17aafa54e2bd4562
Thompson Manda <thompsonmanda08@outlook.com>
2025-07-04 17:31:18 +0200

    hooooooooooooooot

--- src/app/_actions/auth-actions.js
+++ src/app/_actions/auth-actions.js
@@ -509,6 +509,8 @@ export async function getRefreshToken() {
   try {
     const res = await authenticatedApiClient({ url });
 
+    console.log("REFRESH TOKEN RESPONSE ==>", res);
+
     const response = res.data;
 
     const accessToken = response?.token;
