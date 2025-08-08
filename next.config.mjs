d8b59b08ab8a0c0754aedb1caf40a83a50b895d4
Thompson Manda <thompsonmanda08@outlook.com>
2025-08-08 14:24:44 +0200

    fix: update sentry configs

--- next.config.mjs
+++ next.config.mjs
@@ -33,38 +33,38 @@ const nextConfig = {
   experimental: {},
 };
 
-export default withSentryConfig(
-  nextConfig,
+export default withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  /* PRODUCTION SENTRY MONITORING */
-  {
-    // For all available options, see:
-    // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+  org: "sentry",
+  project: "merchant-web-portal",
+  sentryUrl:
+    process.env.NODE_ENV == "production"
+      ? process.env.SENTRY_DEV_URL
+      : process.env.SENTRY_URL,
 
-    authToken: process.env.SENTRY_AUTH_TOKEN,
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
 
-    org: "sentry",
-    project: "payboss-merchant",
-    sentryUrl: "http://102.23.121.155:9000/",
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-    // Only print logs for uploading source maps in CI
-    silent: !process.env.CI,
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
 
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: "/monitoring",
 
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
 
-    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    tunnelRoute: "/monitoring",
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    automaticVercelMonitors: true,
-  },
-);
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
+});
--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,17 +1,18 @@
-// This file configures the initialization of Sentry on the client.
-// The added config here will be used whenever a users loads a page in their browser.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.SENTRY_DNS || process.env.NEXT_PUBLIC_SENTRY_DNS,
+  dsn:
+    process.env.NODE_ENV == "production"
+      ? process.env.SENTRY_DNS || process.env.NEXT_PUBLIC_SENTRY_DNS
+      : process.env.SENTRY_DEV_DNS || process.env.NEXT_PUBLIC_SENTRY_DEV_DNS,
 
   // Add optional integrations for additional features
   integrations: [Sentry.replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
 
   // Define how likely Replay events are sampled.
   // This sets the sample rate to be 10%. You may want this to be 100% while
