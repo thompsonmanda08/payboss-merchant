b68cd592e090c3f3a208529e6c56da58b591bf8d
Thompson Manda <thompsonmanda08@outlook.com>
2025-08-16 16:58:04 +0200

    hot fix

--- src/app/dashboard/[workspaceID]/reports/bills/page.js
+++
@@ -1,8 +0,0 @@
-import BillPaymentReports from './bill-payments-report';
-
-export default async function BillPaymentReportsPage(props) {
-  const params = await props.params;
-  const { workspaceID } = params;
-
-  return <BillPaymentReports workspaceID={workspaceID} />;
-}
